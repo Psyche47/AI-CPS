38285b85dc98cbd5a7e0206ea2bd3ec4b3b09859
Md Ataur Rahman <arsohag20@gmail.com>
2025-02-01 22:35:30 +0100

    Split the dataset into test, training, and activation dataset

---
+++ code/OLSModelTest.py
@@ -0,0 +1,28 @@
+from sklearn.metrics import mean_squared_error, r2_score
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import pickle
+
+df_test = pd.read_csv("data/test_data.csv")
+# print(df_test.head())
+
+Y_test = df_test["CO(GT)"]
+X_test = df_test.drop(columns=["CO(GT)", "AH", "DayOfWeek", "IsWeekend"])
+X_test = sm.add_constant(X_test)
+
+with open("data/currentOlsSolution.pkl", "rb") as file:
+    OLS_model = pickle.load(file)
+
+Y_predict = OLS_model.predict(X_test)
+print(Y_predict)
+
+mse = mean_squared_error(Y_test, Y_predict)
+rmse = np.sqrt(mse)
+r2 = r2_score(Y_test, Y_predict)
+
+print("Mean Squared Error (MSE):", mse)
+
+print("Root Mean Squared Error (RMSE):", rmse)
+
+print("R-squared (R2):", r2)
